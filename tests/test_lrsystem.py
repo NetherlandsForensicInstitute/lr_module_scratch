244b58a12d2d4bba9bfe013e862e93d2270e8b6b
John Braun <j.braun@nfi.nl>
2025-11-21 14:44:59 +0100

    BSZ-213: Add common pytest fixtures to conftest

--- tests/test_lrsystem.py
+++ tests/test_lrsystem.py
@@ -1,39 +1,5 @@
-import pytest
-from lir.data.datasets.synthesized_normal_binary import SynthesizedNormalBinaryData, SynthesizedNormalDataClass
 from lir.data.models import FeatureData
 from lir.lrsystems.lrsystems import LRSystem
-
-from lrmodule.data_types import MarkType, ModelSettings, ScoreType
-from lrmodule.lrsystem import load_lrsystem
-
-
-@pytest.fixture
-def sample_feature_data() -> FeatureData:
-    """Provide FeatureData collection of synthesized normal binary data."""
-    data = SynthesizedNormalBinaryData(
-        data_classes={
-            0: SynthesizedNormalDataClass(mean=-1, std=1, size=100),
-            1: SynthesizedNormalDataClass(mean=1, std=1, size=100),
-        },
-        seed=0,
-    )
-    data = data.get_instances()
-    data = data.replace(features=data.features.flatten())
-
-    return data
-
-
-def test_load_lrsystem():
-    load_lrsystem(ModelSettings(MarkType.FIRING_PIN_IMPRESSION, ScoreType.ACCF))
-
-
-@pytest.fixture
-def trained_lr_system(sample_feature_data: FeatureData) -> LRSystem:
-    """Provide a basic trained LR system model based on specific settings and data."""
-    lrsystem = load_lrsystem(ModelSettings(MarkType.FIRING_PIN_IMPRESSION, ScoreType.ACCF))
-    lrsystem.fit(sample_feature_data)
-
-    return lrsystem
 
 
 def test_run_lrsystem(trained_lr_system: LRSystem, sample_feature_data: FeatureData):
