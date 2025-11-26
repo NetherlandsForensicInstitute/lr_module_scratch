d9d0e70d385012a51bab77046da88d9a89cbcd56
Pim Meulensteen <pimmeulensteen+git@gmail.com>
2025-11-26 17:07:36 +0100

    use aggregation for metrics in experiments / change test to not run whole yaml

--- tests/test_validation_yaml.py
+++ tests/test_validation_yaml.py
@@ -5,7 +5,11 @@ from lir.config.experiment_strategies import parse_experiments_setup
 
 
 def test_validation_yaml():
+    """Test if the validation.yaml file can be parsed correctly.
+    
+    Does not test correctness of the content, only that it can be parsed without errors.
+    Running the whole setup will take too long for a unit test.
+    """
     validation_file = Path(__file__).parent.parent / "validation.yaml"
     setup, _ = parse_experiments_setup(confidence.loadf(validation_file))
-    for _, experiment in setup.items():
-        experiment.run()
+
