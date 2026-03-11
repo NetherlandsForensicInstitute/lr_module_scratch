344a166df53db75dc771f7cae2f3b26567863816
Pim Meulensteen <pimmeulensteen+git@gmail.com>
2026-03-11 15:59:15 +0100

    Implementation of `get_lr_system` and `get_reference_data` (#23)
    * First basic implementation
    
    * update based on review comments
    
    * Add functions to load LR system and reference data from disk
    
    * Update .gitignore, remove test_validation_yaml, and add test_api for loading LR systems
    
    * Update lir package version to 1.4.0

--- lrmodule/__init__.py
+++ lrmodule/__init__.py
@@ -1,13 +1,58 @@
+import pickle
 from pathlib import Path
 
 import numpy as np
 from lir.config.lrsystem_architectures import specific_source
 from lir.data.models import FeatureData, LLRData
+from lir.datasets.feature_data_csv import FeatureDataCsvFileParser
 from lir.lrsystems.lrsystems import LRSystem
 
 from lrmodule import persistence
 from lrmodule.data_types import ModelSettings
 from lrmodule.lrsystem import get_trained_model
+
+
+def get_lr_system(lr_system_folder: Path, file_name: str = "model.pkl") -> LRSystem:
+    """
+    Load a trained LR system from disk from a given folder.
+
+    It is expected that the folder contains a file named "model.pkl" (or another name specified by file_name),
+    which is a pickled LRSystem object. The function loads this object and returns it.
+
+    The system is returned as an instance of the LRSystem class. This class provides an apply method, which can be used
+    to calculate LLRs for given features. These features should be contained in a FeatureData object from lir.
+
+    Example usage:
+    ```
+    from lir.data.models import FeatureData
+
+    lr_system = get_lr_system(Path("path/to/lr_system_folder/"))
+
+    # Create three instances of features, each with two feature values.
+    features = np.array([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]])
+    feature_data = FeatureData(features=features)
+    llr_data = lr_system.apply(feature_data)
+    ```
+    """
+    with (lr_system_folder / file_name).open("rb") as f:
+        return pickle.load(f)  # noqa: S301
+
+
+def get_reference_data(lr_system_folder: Path, file_name: str = "reference_data.csv") -> FeatureData:
+    """Load reference data from disk.
+
+    It is expected that the folder contains a file named "reference_data.csv" (or another name specified by file_name),
+    which is a CSV file containing the reference data. The function loads this data and returns it as a FeatureData
+    object.
+
+    If the data has `n` features, the CSV file should have `n+1` columns. One of the columns should be named
+    "hypothesis", and contain the labels for the data. The other columns should contain the feature values.
+
+    The data is returned as an instance of the FeatureData class from lir. This class has a features and a labels
+    attribute, which can be used to access the feature values and the labels, respectively.
+    """
+    reference_data_file = lr_system_folder / file_name
+    return FeatureDataCsvFileParser(file=reference_data_file, label_column="hypothesis").get_instances()
 
 
 def get_model(settings: ModelSettings, training_data: FeatureData, model_storage_path: Path | None) -> LRSystem:
