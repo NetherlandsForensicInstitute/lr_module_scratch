75b788f54fbf29dcdc37676148e2ab4074b41f61
Pim Meulensteen <pimmeulensteen+git@gmail.com>
2026-03-16 13:03:42 +0100

    Remove unused code (#26)
    Co-authored-by: Pim Meulensteen (DBS) <pim.meulensteen@nfi.nl>

--- lrmodule/__init__.py
+++ lrmodule/__init__.py
@@ -1,15 +1,10 @@
 import pickle
 from pathlib import Path
 
-import numpy as np
 from lir.config.lrsystem_architectures import specific_source
-from lir.data.models import FeatureData, LLRData
+from lir.data.models import FeatureData
 from lir.datasets.feature_data_csv import FeatureDataCsvFileParser
 from lir.lrsystems.lrsystems import LRSystem
-
-from lrmodule import persistence
-from lrmodule.data_types import ModelSettings
-from lrmodule.lrsystem import get_trained_model
 
 
 def get_lr_system(lr_system_folder: Path, file_name: str = "model.pkl") -> LRSystem:
@@ -53,31 +48,6 @@ def get_reference_data(lr_system_folder: Path, file_name: str = "reference_data.
     """
     reference_data_file = lr_system_folder / file_name
     return FeatureDataCsvFileParser(file=reference_data_file, label_column="hypothesis").get_instances()
-
-
-def get_model(settings: ModelSettings, training_data: FeatureData, model_storage_path: Path | None) -> LRSystem:
-    """
-    Obtain a model by loading it from disk, or by fitting it from training data.
-
-    :param settings: model settings
-    :param training_data: training data
-    :param model_storage_path: path where trained LR models are stored
-    :return: a fitted LR system
-    """
-    model = None if not model_storage_path else persistence.load_model(settings, model_storage_path)
-    if not model:
-        model = get_trained_model(settings, training_data)
-        if model_storage_path:
-            persistence.save_model(model, settings, model_storage_path)
-    return model
-
-
-def calculate_llrs(
-    features: np.ndarray, settings: ModelSettings, training_data: FeatureData, model_storage_path: Path | None
-) -> LLRData:
-    """Calculate LLRs after fitting a model with a training set."""
-    model = get_model(settings, training_data, model_storage_path)
-    return model.apply(FeatureData(features=features))
 
 
 # create an alias for the specific source system, since the architecture is identical but the name is misleading
--- tests/test_lrsystem.py
+++
@@ -1,7 +0,0 @@
-from lir.data.models import FeatureData
-from lir.lrsystems.lrsystems import LRSystem
-
-
-def test_run_lrsystem(trained_lr_system: LRSystem, sample_feature_data: FeatureData):
-    llrs = trained_lr_system.apply(sample_feature_data)
-    assert llrs.features.shape == (200, 3)
