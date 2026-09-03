feb1baac56f0b45fbdd1ca4c11d48bb2a625f604
Leen van der Ham <l.van.der.ham@nfi.nl>
2026-09-03 16:00:41 +0200

    working basic version

--- lrmodule/plot_mcmc.py
+++ lrmodule/plot_mcmc.py
@@ -2,7 +2,6 @@ import logging
 from matplotlib import pyplot as plt
 
 from lir.aggregation import Aggregation, AggregationData
-from lir.util import check_not_none
 
 
 LOG = logging.getLogger(__name__)
@@ -23,24 +22,25 @@ class MCMCParameterPlot(Aggregation):
         """
 
         run_name = data.run_name
-        if data.get_full_fit_lrsystem is not None:
-            lrsystem = data.get_full_fit_lrsystem()
-        else:
-            LOG.warning(
-                f'No full-data-fitted model factory available for run `{run_name}`; '
-                f'using split-trained model instead.'
-            )
-            lrsystem = check_not_none(data.lrsystem)
-        mcmc_system = [x[1] for x in lrsystem.lrsystem.pipeline.steps if x[0] == 'mcmc'][0]
+        lrsystem = data.get_full_fit_lrsystem()
 
-        for hypothesis, model in {'h1': mcmc_system.model_h1, 'h2': mcmc_system.model_h2}.items():
+        mcmc_system = [x[1] for x in lrsystem.lrsystem.pipeline.steps if x[0] == 'mcmc']
+        if not mcmc_system:
+            raise ValueError('expected an `mcmc` step in the lrsystem pipeline, but found none.')
+        else:
+            mcmc_system = mcmc_system[0]
+
+        hypothesis_models = {'h1': mcmc_system.model_h1, 'h2': mcmc_system.model_h2}
+        for hypothesis, model in hypothesis_models.items():
             for parameter_name, parameter_values in model.parameter_samples.items():
 
                 plot_name = 'MCMC_parameter-' + hypothesis + '_' + model.distribution + '_' + parameter_name
                 fig, ax = plt.subplots()
 
                 try:
-                    ax.hist(parameter_values)
+                    ax.hist(parameter_values, density=True)
+                    ax.set_xlabel(parameter_name)
+                    ax.set_ylabel('probability density')
                 except ValueError as e:
                     LOG.warning(f'Could not generate plot {plot_name} for run `{run_name}`: {e}')
                     return
