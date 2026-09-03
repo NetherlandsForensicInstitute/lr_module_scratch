716e851a5f9b84d27be4e6db9f3b79a86eebf2b6
Leen van der Ham <l.van.der.ham@nfi.nl>
2026-09-03 16:32:23 +0200

    more format fixes

--- lrmodule/plot_mcmc.py
+++ lrmodule/plot_mcmc.py
@@ -8,7 +8,6 @@ LOG = logging.getLogger(__name__)
 
 
 class MCMCParameterPlot(Aggregation):
-
     # Based on PlotEach and CaseLLRToCsv
 
     def report(self, data: AggregationData) -> None:
@@ -23,31 +22,30 @@ class MCMCParameterPlot(Aggregation):
         run_name = data.run_name
         lrsystem = data.get_full_fit_lrsystem()
 
-        mcmc_system = [x[1] for x in lrsystem.lrsystem.pipeline.steps if x[0] == 'mcmc']
+        mcmc_system = [x[1] for x in lrsystem.lrsystem.pipeline.steps if x[0] == "mcmc"]
         if not mcmc_system:
-            raise ValueError('expected an `mcmc` step in the lrsystem pipeline, but found none.')
+            raise ValueError("expected an `mcmc` step in the lrsystem pipeline, but found none.")
         else:
             mcmc_system = mcmc_system[0]
 
-        hypothesis_models = {'h1': mcmc_system.model_h1, 'h2': mcmc_system.model_h2}
+        hypothesis_models = {"h1": mcmc_system.model_h1, "h2": mcmc_system.model_h2}
         for hypothesis, model in hypothesis_models.items():
             for parameter_name, parameter_values in model.parameter_samples.items():
-
-                plot_name = 'MCMC_parameter-' + hypothesis + '_' + model.distribution + '_' + parameter_name
+                plot_name = "MCMC_parameter-" + hypothesis + "_" + model.distribution + "_" + parameter_name
                 fig, ax = plt.subplots()
 
                 try:
-                    x, y = FFTKDE(bw="silverman").fit(parameter_values).evaluate(2 ** 10)
+                    x, y = FFTKDE(bw="silverman").fit(parameter_values).evaluate(2**10)
                     ax.plot(x, y)
                     ax.set_xlabel(parameter_name)
-                    ax.set_ylabel('probability density')
+                    ax.set_ylabel("probability density")
                 except ValueError as e:
-                    LOG.warning(f'Could not generate plot {plot_name} for run `{run_name}`: {e}')
+                    LOG.warning(f"Could not generate plot {plot_name} for run `{run_name}`: {e}")
                     return
 
-                file_name = data.resolve_path_for_run(f'{plot_name}.png')
+                file_name = data.resolve_path_for_run(f"{plot_name}.png")
 
-                LOG.info(f'Saving plot {plot_name} for run `{run_name}` to {file_name}')
+                LOG.info(f"Saving plot {plot_name} for run `{run_name}` to {file_name}")
                 fig.savefig(file_name)
 
                 plt.close(fig)
