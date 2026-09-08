067cceb60c41c370593d69084c1452aff2823ecc
wb <20113294+wowtor@users.noreply.github.com>
2026-09-08 08:50:13 +0200

    add unit test

--- tests/test_input_data.py
+++ tests/test_input_data.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import numpy as np
 import pytest
-from lir.data.models import FeatureData
+from lir.data.models import FeatureData, PairedFeatureData
 from lir.datasets.feature_data_csv import ExtraField, FeatureDataCsvParser
 from numpy import array
 
@@ -98,6 +98,7 @@ def test_cross_validation_pairs():
         train_sources = set(train.source_ids.flatten())
         test_sources = set(test.source_ids.flatten())
         assert train_sources & test_sources == set(), "sources should not overlap between train and test"
+        assert train_sources | test_sources == set(source_ids.flatten()), "union of train and test should be the full set"
 
         # All pairs in train have both sources in training set
         for pair in train.source_ids:
@@ -108,6 +109,22 @@ def test_cross_validation_pairs():
         for pair in test.source_ids:
             assert pair[0] in test_sources
             assert pair[1] in test_sources
+
+
+def test_pair_count():
+    # generate 10 x 10 = 100 pairs
+    source_ids = np.array(np.meshgrid(np.arange(10), np.arange(10))).T.reshape(-1, 2)
+    labels = source_ids[:,0] == source_ids[:,1]
+    features = np.arange(200).reshape(100, 2, 1).astype(float)
+    data = PairedFeatureData(features=features, hypothesis=labels, source_ids=source_ids, n_trace_instances=1, n_ref_instances=1)
+    strategy = CrossValidationPairs(folds=5, random_state=42)
+    for train, test in strategy.apply(data):
+        assert len(train) == 64
+        assert len(test) == 4
+        assert len(train[train.hypothesis==1]) == 8
+        assert len(test[test.hypothesis==1]) == 2
+        assert len(train[train.hypothesis==0]) == 8*7
+        assert len(test[test.hypothesis==0]) == 2
 
 
 def test_cross_validation_pairs_invalid_source_ids():
